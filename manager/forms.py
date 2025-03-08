6137791accf07f7d2f2df9a0a98d7d12cbc7c813
Yura Chornyi <chorny.yura@gmail.com>
2025-03-08 16:54:18 +0200

    feat: add validation for equipment serial number

--- manager/forms.py
+++ manager/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 from manager.models import Staff, Equipment
 
 
 class StaffForm(UserCreationForm):
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
+    email = forms.EmailField(max_length=100, required=True)
+
     class Meta(UserCreationForm.Meta):
         model = Staff
         fields = UserCreationForm.Meta.fields + (
@@ -14,6 +19,10 @@ class StaffForm(UserCreationForm):
 
 
 class StaffUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
+    email = forms.EmailField(max_length=100, required=True)
+
     class Meta:
         model = Staff
         fields = ("role", "email", "first_name", "last_name")
@@ -23,8 +32,25 @@ class EquipmentForm(forms.ModelForm):
     assigned_to = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
         widget=forms.CheckboxSelectMultiple,
+        required=False,
     )
 
     class Meta:
         model = Equipment
         fields = "__all__"
+
+    def clean_internal_serial_number(self):
+        serial_number = self.cleaned_data["internal_serial_number"]
+        return validate_serial_number(serial_number)
+
+
+# validation for internal serial number of equipment
+def validate_serial_number(serial_number):
+    if len(serial_number) != 10:
+        raise ValidationError("Serial number should consist of 10 characters")
+    elif not serial_number[:3].isupper() or not serial_number[:3].isalpha():
+        raise ValidationError("First 3 characters should be uppercase letters")
+    elif not serial_number[3:].isdigit():
+        raise ValidationError("Last 7 characters should be digits")
+
+    return serial_number
