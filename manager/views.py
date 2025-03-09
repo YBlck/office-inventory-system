b1f6784f7739485d9fd4717dea246ac0499f8cae
Yura Chornyi <chorny.yura@gmail.com>
2025-03-09 02:02:16 +0200

    style: format to flake8

--- manager/views.py
+++ manager/views.py
@@ -126,7 +126,6 @@ class EquipmentListView(generic.ListView):
     model = Equipment
     paginate_by = 10
 
-
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(EquipmentListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
